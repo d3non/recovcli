1dc61816999ec9d9f9a395274c15986fae495269
d3non <peps.valle@gmail.com>
2019-08-10 01:02:46 -0500

    adding concurrently

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-/* Serve up static assets (usually on heroku)
+
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-}*/
+}
 
 /*const mongoose = require("mongoose");
 
